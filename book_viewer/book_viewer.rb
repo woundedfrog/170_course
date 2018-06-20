baac8d90193689edba18c0fefb1c176293489a2a
Bernard Maisel <ntjust26@gmail.com>
2018-06-20 16:00:14 +0900

    Basic first tasks done
    Added chapters in menu

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,10 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
+  erb :home
+end
