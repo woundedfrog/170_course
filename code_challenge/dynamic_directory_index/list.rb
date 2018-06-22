1732928c5443f99168f62b07add7d2e177894c08
Bernard Maisel <ntjust26@gmail.com>
2018-06-22 17:36:31 +0900

    Added list.rb code
    Includes a get function to the sub folders in the public folder.

--- code_challenge/dynamic_directory_index/list.rb
+++ code_challenge/dynamic_directory_index/list.rb
@@ -1,3 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+
+get "/" do
+  @files = Dir.glob("public/*").map { |file| File.basename(file) }.sort
+  @files.reverse! if params[:sort] == "desc"
+  erb :list
+end
+
+get "/fonts" do
+  @sub_files = Dir.glob("public/fonts/*").map { |file| File.basename(file) }.sort
+  @sub_files.reverse! if params[:sort] == "desc"
+  erb :sub_list
+end
